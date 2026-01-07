07ed0bfb5b8ea16010fadcd8304bba863c38906e
Alexis Lerch <alexis.lerch123@gmail.com>
2026-01-07 12:29:17 -0300

    primera conexion con la api y mostrando los primeros datos

--- next.config.ts
+++ next.config.ts
@@ -8,8 +8,12 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "assets.coingecko.com",
       },
-    ]
-  }
+      {
+        protocol: "https",
+        hostname: "coin-images.coingecko.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
